8e4d236f401ec9aca45828d4e843218d396e8677
Vimal Kaiprath <vkaiprath@ibizsoftinc.com>
2024-04-01 14:49:27 +0530

    Initial commit

---
+++ src/test/java/com/vk/demojdbc/SpringJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.demojdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdbcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
